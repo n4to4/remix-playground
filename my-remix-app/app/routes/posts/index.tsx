687ba24b3eca397a37c63b95a4175479927e63e7
n4to4 <n4to4k@gmail.com>
2021-11-24 19:17:16 +0900

    loading

--- my-remix-app/app/routes/posts/index.tsx
+++ my-remix-app/app/routes/posts/index.tsx
@@ -1,7 +1,12 @@
-import { useLoaderData } from "remix";
+import { Link, useLoaderData } from "remix";
+
+type Post = {
+  slug: string;
+  title: string;
+}
 
 export let loader = () => {
-  return [
+  let posts: Post[] = [
     {
       slug: "my-first-post",
       title: "My First Post"
@@ -11,14 +16,22 @@ export let loader = () => {
       title: "A Mixtape I Made Just For You"
     }
   ];
+  return posts;
 };
 
 export default function Posts() {
-  let posts = useLoaderData();
+  let posts = useLoaderData<Post[]>();
   console.log(posts);
   return (
     <div>
       <h1>Posts</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.slug}>
+            <Link to={post.slug}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
